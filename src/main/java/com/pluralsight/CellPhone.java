6a790c5fc249a76ea7de034cf0101f785099339f
kdorcelien <kdorcelien.kd@gmail.com>
2025-09-30 15:47:27 -0400

    Mid commit

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -1,4 +1,57 @@
 package com.pluralsight;
 
 public class CellPhone {
+    private double serialNumber;
+    private String model;
+    private String carrier;
+    private String phoneNumber;
+    private String owner;
+
+    public CellPhone() {
+        this.serialNumber = 0;
+        this.model = "";
+        this.carrier = "";
+        this.phoneNumber = "";
+        this.owner = "";
+
+    }
+    public double getSerialNumber(){
+        return this.serialNumber;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public String getCarrier() {
+        return this.carrier;
+    }
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    public String getOwner() {
+        return this.owner;
+    }
+
+    public void setSerialNumber(double serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
 }
--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,7 +1,11 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class CellPhoneApplication {
     public static void main(String[] args) {
+CellPhone phone = new CellPhone();
+       // phone.setCarrier();
 
     }
 }
