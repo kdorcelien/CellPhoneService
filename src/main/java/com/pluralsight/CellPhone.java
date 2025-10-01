53ee668970cbddf351c221fe968c92a3ebbc4dfd
kdorcelien <kdorcelien.kd@gmail.com>
2025-10-01 14:28:08 -0400

    Day 2 commit

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -1,8 +1,5 @@
 package com.pluralsight;
 
-import java.sql.SQLOutput;
-import java.util.Scanner;
-
 public class CellPhone {
     private int serialNumber;
     private String model;
@@ -16,8 +13,17 @@ public class CellPhone {
         this.carrier = "";
         this.phoneNumber = "";
         this.owner = "";
-
     }
+
+    public CellPhone(int serialNumber, String model, String carrier, String phoneNumber, String owner ){
+        this.serialNumber = serialNumber;
+        this.model = model;
+        this.carrier = carrier;
+        this.phoneNumber = phoneNumber;
+        this.owner = owner;
+
+        }
+
     public int getSerialNumber(){
         return this.serialNumber;
     }
@@ -59,9 +65,10 @@ public class CellPhone {
     }
 
     public void dial(String phoneNumber){
-        Scanner input2 = new Scanner(System.in);
-        System.out.println("Enter phone number: ");
-        String pNumber2 = input2.nextLine();
-        System.out.println(owner +"'s phone is calling " + pNumber2);
+        System.out.println(owner +"'s phone is calling " + phoneNumber);
+    }
+
+    public void dial(CellPhone phone3){
+        System.out.println(owner +"'s phone is calling " + phoneNumber);
     }
 }
--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -7,6 +7,14 @@ public class CellPhoneApplication {
         Scanner input = new Scanner(System.in);
         CellPhone phone = new CellPhone();
         CellPhone phone2 = new CellPhone();
+        CellPhone phone3 = new CellPhone(999, "iPhone", "Visibe", "0967", "Carl Max");
+
+        phone2.setSerialNumber(888);
+        phone2.setPhoneNumber("711");
+        phone2.setOwner("Help Line");
+        phone2.setCarrier("T-Mobile");
+        phone2.setModel("Landline");
+
 
         System.out.println("What is the Phone serial number?: ");
         int sNumber = input.nextInt();
@@ -34,9 +42,20 @@ public class CellPhoneApplication {
         phone.setOwner(pOwner);
         System.out.println(phone.getOwner());
 
-     //  public static void display(){}
+        phone2.dial("(999) 555-1202");
+        phone.dial(phone3.getPhoneNumber());
 
+        display(phone);
+        display(phone2);
+        display(phone3);
 
         }
+    public static void display(CellPhone phone) {
+        System.out.println("Serial Number: " + phone.getSerialNumber());
+        System.out.println("Model: " + phone.getModel());
+        System.out.println("Carrier: " + phone.getCarrier());
+        System.out.println("Phone Number: " + phone.getPhoneNumber());
+        System.out.println("Owner: " + phone.getOwner());
+          }
     }
 
